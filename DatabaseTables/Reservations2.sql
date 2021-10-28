6943234be7f7359b9fe31629ea790932562941e6
Adel <adelheskestad@yahoo.no>
2021-10-28 14:57:01 +0200

    Added Date and quick random dates to table

--- DatabaseTables/Reservations2.sql
+++ DatabaseTables/Reservations2.sql
@@ -9,6 +9,7 @@ create table Reservations
     userID int not null ,
     ReservationPrice varchar(255),
     equipmentID int not null,
+    rentStartDate date not null,
     constraint Reservations
         primary key (ReservationID),
     constraint FK_userID
@@ -89,6 +90,31 @@ values ('23','','5','Egne regler','6');
 insert into Reservations (reservationid, reservationdays, userid, reservationprice, equipmentID)
 values ('24','','4','Egne regler','8');
 
+# Innsetting av dato
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-29' WHERE t.ReservationID = 11;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-18' WHERE t.ReservationID = 19;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-18' WHERE t.ReservationID = 24;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-12' WHERE t.ReservationID = 17;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-05' WHERE t.ReservationID = 3;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-28' WHERE t.ReservationID = 2;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-26' WHERE t.ReservationID = 15;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-13' WHERE t.ReservationID = 20;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-23' WHERE t.ReservationID = 9;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-14' WHERE t.ReservationID = 23;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-08' WHERE t.ReservationID = 21;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-18' WHERE t.ReservationID = 1;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-07' WHERE t.ReservationID = 18;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-22' WHERE t.ReservationID = 4;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-12' WHERE t.ReservationID = 13;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-19' WHERE t.ReservationID = 12;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-14' WHERE t.ReservationID = 22;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-15' WHERE t.ReservationID = 10;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-11' WHERE t.ReservationID = 8;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-11' WHERE t.ReservationID = 7;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-18' WHERE t.ReservationID = 14;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-31' WHERE t.ReservationID = 16;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-19' WHERE t.ReservationID = 6;
+UPDATE mytestdb.Reservations t SET t.rentStartDate = '2021-10-18' WHERE t.ReservationID = 5;
 
 
 
