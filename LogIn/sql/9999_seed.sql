1d659be8dbe6240bfa19f5696d4da2e83988e38d
Jon Grythe Stødle <jon@stodle.net>
2021-11-14 21:37:50 +0100

    Fix casing

--- LogIn/sql/9999_seed.sql
+++ LogIn/sql/9999_seed.sql
@@ -17,35 +17,35 @@ values (1, 'admin'),
        (6, 'admin'),
        (7, 'admin');
 
-INSERT INTO equipment
-    (name, kind, price, rent_start_day, comment)
-VALUES ('Eksentersliper 230VAC', 'diverse «småutstyr»', 2000, 3, null),
-       ('Båndsliper 230VAC', 'diverse «småutstyr»', 2000, 3, null),
+insert into equipment
+    (name, category, price, rent_start_day, comment)
+values ('Eksentersliper 230VAC', 1, 2000, 3, null),
+       ('Båndsliper 230VAC', 1, 2000, 3, null),
        ('Høvel 230VAC', 'diverse «småutstyr» ', 2000, 3, null),
-       ('Gjære-/kombisag 230VAC', 'diverse «småutstyr»', 2000, 3, null),
-       ('9’ vinkelsliper for stein og betong', 'diverse «småutstyr»', 2000, 3, null),
-       ('Meislemaskin, 230VAC', 'diverse «småutstyr»', 2000, 3, null),
-       ('Slagdrill, Batteri, Milwaukee', 'diverse «småutstyr»', 2000, 3, null),
-       ('Kantklipper - bensin', 'diverse «småutstyr»', 2000, 3, null),
-       ('Kompressor, 230VAC', 'diverse «småutstyr»', 2000, 3, null),
-       ('Spikerpistol, stor (trykkluft)', 'Spikerpistoler o.l', 2000, 3, null),
-       ('Spikerpistol, liten (trykkluft)', 'Spikerpistoler o.l', 2000, 3, null),
-       ('Spikerpistol, stor (Milwaukee)', 'Spikerpistoler o.l', 2000, 3, null),
-       ('Spikerpistol, mellom (Milwaukee)', 'Spikerpistoler o.l', 2000, 3, null),
-       ('Spikerpistol, liten (Milwaukee) ', 'Spikerpistoler o.l', 2000, 3, null),
-       ('Vedkløyver, Bensindrevet', 'Utstyr for vedhogst:', 5000, 3, null),
-       ('Fliskutter for trevirke', 'Utstyr for vedhogst:', 5000, 3, null),
-       ('Tilhenger, boggi; RD 5702', 'Tilhengere', 5000, 3, null),
+       ('Gjære-/kombisag 230VAC', 1, 2000, 3, null),
+       ('9’ vinkelsliper for stein og betong', 1, 2000, 3, null),
+       ('Meislemaskin, 230VAC', 1, 2000, 3, null),
+       ('Slagdrill, Batteri, Milwaukee', 1, 2000, 3, null),
+       ('Kantklipper - bensin', 1, 2000, 3, null),
+       ('Kompressor, 230VAC', 1, 2000, 3, null),
+       ('Spikerpistol, stor (trykkluft)', 2, 2000, 3, null),
+       ('Spikerpistol, liten (trykkluft)', 2, 2000, 3, null),
+       ('Spikerpistol, stor (Milwaukee)', 2, 2000, 3, null),
+       ('Spikerpistol, mellom (Milwaukee)', 2, 2000, 3, null),
+       ('Spikerpistol, liten (Milwaukee) ', 2, 2000, 3, null),
+       ('Vedkløyver, Bensindrevet', 3, 5000, 3, null),
+       ('Fliskutter for trevirke', 3, 5000, 3, null),
+       ('Tilhenger, boggi; RD 5702', 4, 5000, 3, null),
        ('Tilhenger, liten, RC 8834
 Kassemål LxBxH: 197x153x29cm
-Nyttelast max. 645kg', 'Tilhengere', 5000, 3, null),
+Nyttelast max. 645kg', 4, 5000, 3, null),
        ('Tilhenger, skap, JP2591
 Kassemål innv. LxBxH: 292x154x194cm
 (Dørkarm BxH: 146x190cm)
-Nyttelast: 1090 kg', 'Tilhengere', 5000, 3, null),
+Nyttelast: 1090 kg', 4, 5000, 3, null),
        ('Personløfter, arb.høyde 12m.
-SWL 200 kg, 230 VAC', 'Større utstyr', 10000, 0, null),
-       ('Strømaggregat, 3.7 kW (Diesel)', 'Større utstyr', 5000, 3, null),
-       ('Dekkomleggingsmaskin', 'Større utstyr', -1, 0, 'Egne regler'),
-       ('Avbalanseringsmaskin', 'Større utstyr', -1, 0, 'Egne regler'),
-       ('Leilighet på Hovden', 'Større utstyr', -1, 0, 'Egne regler. Kontakt resepsjonen hos AMV');
+SWL 200 kg, 230 VAC', 5, 10000, 0, null),
+       ('Strømaggregat, 3.7 kW (Diesel)', 5, 5000, 3, null),
+       ('Dekkomleggingsmaskin', 5, -1, 0, 'Egne regler'),
+       ('Avbalanseringsmaskin', 5, -1, 0, 'Egne regler'),
+       ('Leilighet på Hovden', 5, -1, 0, 'Egne regler. Kontakt resepsjonen hos AMV');
