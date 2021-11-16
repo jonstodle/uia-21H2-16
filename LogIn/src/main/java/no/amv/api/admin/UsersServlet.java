0c998cb5ab9217b15319c88b1b3a8725c664c197
Jon Grythe Stødle <jon@stodle.net>
2021-11-16 23:15:49 +0100

    Only set a password if one is provided when updating a user

--- LogIn/src/main/java/no/amv/api/admin/UsersServlet.java
+++ LogIn/src/main/java/no/amv/api/admin/UsersServlet.java
@@ -13,7 +13,7 @@ public class UsersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var editParam = req.getParameter("edit");
         if (editParam != null) {
-            if (editParam != "") {
+            if (!editParam.equals("")) {
                 var editId = Integer.parseInt(editParam);
                 var user = User.getById(editId);
                 req.setAttribute("user", user);
@@ -42,14 +42,22 @@ public class UsersServlet extends HttpServlet {
         if (editParam != null) {
             var user = new User(0, "", "", "", User.generateSalt());
 
-            if (editParam != "") {
+            if (!editParam.equals("")) {
                 var editId = Integer.parseInt(editParam);
                 user = User.getById(editId);
             }
 
             user.setName(req.getParameter("name"));
             user.setEmail(req.getParameter("email"));
-            user.setPassword(req.getParameter("password"));
+
+            var password = req.getParameter("password");
+            if (password != null && password.equals("")) {
+                user.setPassword(password);
+            } else if (user.getPassword().equals("")) {
+                // Generate a random password if user has no password,
+                // and a new one hasn't been set
+                user.setPassword(User.generateSalt());
+            }
 
             user.save();
 
